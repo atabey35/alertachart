fb4e5eddcd11eb88da57fde2fa5c0e542f5e7d4f
atabey35 <kriptokirmizi@gmail.com>
2025-10-25 00:10:39 +0300

    Fix watchlist scroll - add overflow-hidden to parent
    - Added overflow-hidden to watchlist parent container
    - Prevents whole page scrolling when scrolling watchlist
    - Scroll now properly contained to coin list area only
    - Category filters scroll horizontally, coin list scrolls vertically

--- components/Watchlist.tsx
+++ components/Watchlist.tsx
@@ -308,7 +308,7 @@ export default function Watchlist({ onSymbolClick, currentSymbol, marketType = '
   }
 
   return (
-    <div className="bg-gray-900 border-l border-gray-800 flex flex-col relative h-full" style={{ width: `${width}px`, minWidth: `${width}px` }}>
+    <div className="bg-gray-900 border-l border-gray-800 flex flex-col relative h-full overflow-hidden" style={{ width: `${width}px`, minWidth: `${width}px` }}>
       {/* Resize Handle */}
       <div
         className="absolute left-0 top-0 bottom-0 w-1 cursor-ew-resize hover:bg-blue-500 transition-colors z-50 group"
