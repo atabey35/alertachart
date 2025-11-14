0defc8af3af55774d9832d924f4b150eba4da85b
atabey35 <kriptokirmizi@gmail.com>
2025-11-14 17:00:54 +0300

    fix: Configure Google Auth plugin for Android
    - Add GoogleAuth serverClientId to capacitor.config.ts
    - Add server_client_id to Android strings.xml
    - Keep MainActivity simple (auto plugin discovery)
    - Fix GoogleSignInClient null pointer exception

--- android/app/src/main/java/com/kriptokirmizi/alerta/MainActivity.java
+++ android/app/src/main/java/com/kriptokirmizi/alerta/MainActivity.java
@@ -1,14 +1,5 @@
 package com.kriptokirmizi.alerta;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Initialize Google Auth
-        GoogleAuth.initialize(this);
-    }
-}
+public class MainActivity extends BridgeActivity {}
